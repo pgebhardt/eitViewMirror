4edeb7595587fd3c815a44dc30b84618e97e4947
Schansge <grosser.knuff@googlemail.com>
2014-02-05 23:12:16 +0100

    basic electrodes drawing added

--- EITViewMirror/ESTViewController.m
+++ EITViewMirror/ESTViewController.m
@@ -8,26 +8,14 @@
 
 #import "ESTViewController.h"
 
-typedef struct {
-    float Position[3];
-    float Color[4];
-} Vertex;
-
-const Vertex Vertices[] = {
-    {{1, -1, 0}, {1, 0, 0, 1}},
-    {{1, 1, 0}, {0, 1, 0, 1}},
-    {{-1, 1, 0}, {0, 0, 1, 1}},
-    {{-1, -1, 0}, {0, 0, 0, 1}}
-};
-
-const GLubyte Indices[] = {
-    0, 1, 2,
-    2, 3, 0
+const GLfloat Vertices[] = {
+    1, -1, 0,
+    1, 1, 0,
+    -1, 1, 0
 };
 
 @implementation ESTViewController {
     GLuint _vertexBuffer;
-    GLuint _indexBuffer;
     float _rotation;
 }
 
@@ -36,15 +24,25 @@ const GLubyte Indices[] = {
     
     // initialize shaders
     self.baseEffect = [[GLKBaseEffect alloc] init];
+    self.baseEffect.useConstantColor = YES;
+    self.baseEffect.constantColor = GLKVector4Make(0.0, 0.0, 0.0, 1.0);
+    
+    // fill vertex buffer
+    GLfloat* electrodes = malloc(sizeof(GLfloat) * 2 * 2 * 36);
+    float delta_angle = 2.0 * M_PI / 36.0;
+    for (int i = 0; i < 36; ++i) {
+        float angle = (float)i * delta_angle;
+        electrodes[0 * 2 + 0 + i * 2 * 2] = cosf(angle);
+        electrodes[0 * 2 + 1 + i * 2 * 2] = sinf(angle);
+        electrodes[1 * 2 + 0 + i * 2 * 2] = cosf(angle + 0.1);
+        electrodes[1 * 2 + 1 + i * 2 * 2] = sinf(angle + 0.1);
+    }
     
     // init vertex and index buffer for square
     glGenBuffers(1, &_vertexBuffer);
     glBindBuffer(GL_ARRAY_BUFFER, _vertexBuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
-    
-    glGenBuffers(1, &_indexBuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, _indexBuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 2 * 2 * 36, electrodes, GL_STATIC_DRAW);
+    free(electrodes);
 }
 
 -(void)tearDown {
@@ -52,7 +50,6 @@ const GLubyte Indices[] = {
     
     // delete vertex buffer
     glDeleteBuffers(1, &_vertexBuffer);
-    glDeleteBuffers(1, &_indexBuffer);
     
     // release shadres
     self.baseEffect = nil;
@@ -86,14 +83,10 @@ const GLubyte Indices[] = {
     
     // draw square
     glBindBuffer(GL_ARRAY_BUFFER, _vertexBuffer);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _indexBuffer);
     
     glEnableVertexAttribArray(GLKVertexAttribPosition);
-    glVertexAttribPointer(GLKVertexAttribPosition, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const GLvoid*)offsetof(Vertex, Position));
-    glEnableVertexAttribArray(GLKVertexAttribColor);
-    glVertexAttribPointer(GLKVertexAttribColor, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const GLvoid*)offsetof(Vertex, Color));
-    
-    glDrawElements(GL_TRIANGLES, sizeof(Indices)/sizeof(Indices[0]), GL_UNSIGNED_BYTE, 0);
+    glVertexAttribPointer(GLKVertexAttribPosition, 2, GL_FLOAT, GL_FALSE, 0, 0);
+    glDrawArrays(GL_LINES, 0, 2 * 36);
 }
 
 #pragma mark - GLKViewController
@@ -101,11 +94,11 @@ const GLubyte Indices[] = {
 -(void)update {
     // set projection matrix
     float aspect = fabsf(self.view.bounds.size.width / self.view.bounds.size.height);
-    GLKMatrix4 projectionMatrix = GLKMatrix4MakePerspective(GLKMathDegreesToRadians(65.0), aspect, 4.0, 10.0);
+    GLKMatrix4 projectionMatrix = GLKMatrix4MakePerspective(GLKMathDegreesToRadians(65.0), aspect, 0.0, 10.0);
     self.baseEffect.transform.projectionMatrix = projectionMatrix;
     
     // set model view matrix
-    GLKMatrix4 modelViewMatrix = GLKMatrix4MakeTranslation(0.0, 0.0, -6.0);
+    GLKMatrix4 modelViewMatrix = GLKMatrix4MakeTranslation(0.0, 0.0, -3.0);
     _rotation += 90.0 * self.timeSinceLastUpdate;
     modelViewMatrix = GLKMatrix4Rotate(modelViewMatrix, GLKMathDegreesToRadians(_rotation), 0.0, 0.0, 1.0);
     self.baseEffect.transform.modelviewMatrix = modelViewMatrix;
